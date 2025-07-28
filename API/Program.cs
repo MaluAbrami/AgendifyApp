ee09e56681dd66889ea8ea1fb48c8f9b421ba2ca
Malu Abrami <marialuizaabrami@gmail.com>
2025-07-28 18:16:52 -0300

    add validação de exceção global

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using API.Controllers;
 using API.Extensions;
+using Application.Response;
 using Domain.Enums;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +19,32 @@ builder.AddInjections();
 builder.AddRepositories();
 
 var app = builder.Build();
+
+app.UseExceptionHandler(appBuilder =>
+{
+    appBuilder.Run(async context =>
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var error = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        var response = new BaseResponse<object>
+        {
+            ResponseInfo = new ResponseInfo
+            {
+                Title = "Erro interno no servidor",
+                ErrorDescription = $"Ocorreu um erro inesperado: {error}",
+                HttpStatus = 500
+            },
+            Value = null
+        };
+        
+        Console.WriteLine(error?.ToString());
+
+        await context.Response.WriteAsJsonAsync(response);
+    });
+});
 
 using (var scope = app.Services.CreateScope())
 {
